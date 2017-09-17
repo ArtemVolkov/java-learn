5f5b5f02c64242a5b178c60447687f3975bb02fc
ArtemVolkov <awer-1@mail.ru>
2017-09-17 19:30:23 +0300

    create Calculator

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator{
+	public static int add(int first,int second){
+		return first+second;
+	}
+	public int subtraction(int first,int second){
+		return first-second;
+	}
+	public static void main(String[] arg){
+		System.out.println(add(5,6));
+	}
+
+}
